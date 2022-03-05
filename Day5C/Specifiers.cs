bfdb12e8b5afeb0edf668a4a05af908abe23b315
Lakshita-Garg <lakshita.garg@ibm.com>
2022-03-05 19:37:20 +0530

    Task 1: Structures in C# Task 2: Access specifiers in C#

---
+++ Day5C/Specifiers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+public class Specifiers
+{
+    private string Name;
+    private int empId;
+    public void getData(string name, int id)
+    {
+        Name = name;
+        empId = id;
+    }
+    public void showData()
+    {
+        Console.WriteLine("Name of the employee: " + Name);
+        Console.WriteLine("Id of the employee: " + empId);
+    }
+}
+class mainClass
+{
+    static void Main(string[] args)
+    {
+        Specifiers specs = new Specifiers();
+        specs.getData("Lakshi", 10);
+        specs.showData();
+        Console.ReadLine();
+    }
+}
+
---
+++ Day5C/Structures.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public struct Book
+{
+    public string Name;
+    public int Id;
+    public void getData(string name, int id)
+    {
+        Name = name;
+        Id = id;
+    }
+    public void showValues()
+    {
+        Console.WriteLine("Name of the book: " + Name);
+        Console.WriteLine("ID of the book: " + Id);
+    }
+}
+class Structures
+{
+    static void Main(string[] args)
+    {
+        Book b1 = new Book();
+        Book b2 = new Book();
+        b1.getData("Twilight",10);
+        b2.getData("Vampire Diaries", 20);
+        Console.WriteLine("Details of 1st book:");
+        b1.showValues();
+        Console.WriteLine();
+        Console.WriteLine("Details of 2nd book:");
+        b2.showValues();
+        Console.ReadLine();
+
+    }
+}
+
